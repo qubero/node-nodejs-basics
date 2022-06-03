9a2f40aece74c64fd06f8ed805503dbbdd864d3e
wolsos-v <mamaev757@gmail.com>
2022-06-03 17:04:55 +0300

    feat: implement streams/transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,21 @@
+import { Transform } from 'stream';
+import { pipeline } from 'stream/promises';
+
 export const transform = async () => {
-    // Write your code here 
+  try {
+    const rs = process.stdin;
+    const ws = process.stdout;
+
+    const ts = new Transform({
+      transform(chunk, _, cb) {
+        cb(null, String(chunk).split('').reverse().join(''));
+      },
+    });
+
+    await pipeline(rs, ts, ws);
+  } catch (err) {
+    console.error(err.name, '-', err.message);
+  }
 };
+
+transform();
