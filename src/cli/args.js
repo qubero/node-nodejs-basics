3fb7a627d24fab5b482eedfaef75d7b50ba58bcb
wolsos-v <mamaev757@gmail.com>
2022-06-04 13:14:15 +0300

    feat: implement cli/args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,20 @@
 export const parseArgs = () => {
-    // Write your code here 
+  try {
+    const processArgs = process.argv;
+    const prefix = '--';
+    const argVars = [];
+
+    for (const [i, v] of processArgs.entries()) {
+      if (v.startsWith(prefix)) {
+        const argVar = `${v.slice(2)} is ${processArgs[i + 1]}`;
+        argVars.push(argVar);
+      }
+    }
+
+    console.log(argVars.join(', '));
+  } catch (err) {
+    console.error(err.name, '-', err.message);
+  }
 };
+
+parseArgs();
