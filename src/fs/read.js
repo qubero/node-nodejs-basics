f45883653c865e7341936992009aa77cdefadc81
wolsos-v <mamaev757@gmail.com>
2022-06-05 15:49:49 +0300

    feat: implement fs/read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,34 @@
-export const read = async () => {
-    // Write your code here 
+import { access, readFile } from 'fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const isExist = async (path) => {
+  try {
+    await access(path);
+    return true;
+  } catch (err) {
+    return false;
+  }
 };
+
+export const read = async () => {
+  const fileFolder = 'files';
+  const fileName = 'fileToRead.txt';
+  const filePath = join(__dirname, fileFolder, fileName);
+
+  try {
+    if (!(await isExist(filePath))) {
+      throw new Error('FS operation failed');
+    }
+
+    const content = await readFile(filePath, 'utf8');
+    console.log(content);
+  } catch (err) {
+    console.error(err.name, '-', err.message);
+  }
+};
+
+read();
